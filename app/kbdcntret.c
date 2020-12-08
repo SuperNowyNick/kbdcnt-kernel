078650c5f68a14f6bff6933dce053957e0adcb61
SuperNowyNick <10292842+SuperNowyNick@users.noreply.github.com>
2020-12-08 14:13:26 +0100

    Refactor code to match kernel coding style

--- app/kbdcntret.c
+++ app/kbdcntret.c
@@ -5,54 +5,51 @@
 
 #include "../module/kbdcnt.h"
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-	if(argc<=1)
-	{
-		printf("Usage: kbdcntret -options\n\t-r - reset counter\n\t-c - get key press counter\n\t-t - get timestamp of last counter reset\n");
-		return 0;
-	}
-	int file_desc = open("/dev/kbdcnt", 0);
-	if(file_desc == 0)
-	{
-		printf("Error. Cannot open device file\n");
-		return 1;
-	}
-	int ret=0;
-	for(int i=1; i<argc; i++)
-	{
-		if(!strcmp(argv[i], "-r"))
-		{
-			ret = ioctl(file_desc, IOCTL_RST, NULL);
-			if(ret<0){
-				printf("Error. Ioctl get count returned %d\n", ret);
-				return 1;
-			}
-		}
-		if(!strcmp(argv[i], "-c"))
-		{
-			unsigned long long count = 0;
-			ret = ioctl(file_desc, IOCTL_GET_CNT, &count);
-			if(ret<0){
-				printf("Error. Ioctl get count returned %d\n", ret);
-				return 1;
-			}
-			printf("Key press count: %llu\n", count);
-		}
-		if(!strcmp(argv[i], "-t"))
-		{
-			signed long long timens=0;
-			ret = ioctl(file_desc, IOCTL_GET_TIME, &timens);
-			if(ret<0){
-				printf("Error. Ioctl get time returned %d\n", ret);
-				return 1;
-			}
-			time_t time = timens/1000000000;
-			struct tm* tm_info = localtime(&time);
-			char buffer[26];
-			strftime(buffer, 26, "%F %T", tm_info);
-			printf("Last reset time: %s\n", buffer);
-		}
-	}
-	return 0;
+        if (argc <= 1) {
+                printf("Usage: kbdcntret -options\n\t-r - reset counter\n\t-c - get key press counter\n\t-t - get timestamp of last counter reset\n");
+                return 0;
+        }
+        int file_desc = open("/dev/kbdcnt", 0);
+        if (file_desc == 0) {
+                printf("Error. Cannot open device file\n");
+                return 1;
+        }
+        int ret = 0;
+        for (int i = 1; i < argc; i++) {
+                if (!strcmp(argv[i], "-r")) {
+                        ret = ioctl(file_desc, IOCTL_RST, NULL);
+                        if (ret < 0) {
+                                printf("Error. Ioctl get count returned %d\n",
+                                               ret);
+                                return 1;
+                        }
+                }
+                if (!strcmp(argv[i], "-c")) {
+                        unsigned long long count = 0;
+                        ret = ioctl(file_desc, IOCTL_GET_CNT, &count);
+                        if (ret < 0) {
+                                printf("Error. Ioctl get count returned %d\n",
+                                                ret);
+                                return 1;
+                        }
+                        printf("Key press count: %llu\n", count);
+                }
+                if (!strcmp(argv[i], "-t")) {
+                        signed long long timens = 0;
+                        ret = ioctl(file_desc, IOCTL_GET_TIME, &timens);
+                        if (ret < 0) {
+                                printf("Error. Ioctl get time returned %d\n",
+                                                ret);
+                        return 1;
+                }
+                time_t time = timens / 1000000000;
+                struct tm *tm_info = localtime(&time);
+                char buffer[26];
+                strftime(buffer, 26, "%F %T", tm_info);
+                printf("Last reset time: %s\n", buffer);
+                }
+        }
+        return 0;
 }
--- module/kbdcnt.c
+++ module/kbdcnt.c
@@ -21,105 +21,107 @@ static struct device *kbdDevice;
 
 static int kbdDeviceCount = 0;
 
-static irqreturn_t kbdIrqHandler(int irq, void* dev_id, struct pt_regs *regs)
+static irqreturn_t kbdIrqHandler(int irq, void *dev_id, struct pt_regs *regs)
 {
-	unsigned char scancode = inb(0x60);
-	if(scancode<128)
-		kbdKeyCount++;
-	return (irqreturn_t)IRQ_HANDLED; 
+        unsigned char scancode = inb(0x60);
+        if (scancode < 128)
+                kbdKeyCount++;
+        return (irqreturn_t)IRQ_HANDLED; 
 }
 
-static long kbdDeviceIoctl(struct file* file, unsigned int num, unsigned long param)
+static long kbdDeviceIoctl(struct file *file, unsigned int num,
+                unsigned long param)
 {
-	signed long long time;
-	switch(num)
-	{
-		case IOCTL_RST:
-			kbdKeyCount = 0;
-			kbdResetTime = ktime_get_real();
-			break;
-		case IOCTL_GET_CNT:
-			if(copy_to_user((unsigned long long*)param, &kbdKeyCount, sizeof(unsigned long long)))
-				return -EFAULT;
-			break;
-		case IOCTL_GET_TIME:
-			time = ktime_to_ns(kbdResetTime);
-			if(copy_to_user((signed long long*)param, &time, sizeof(signed long long)))
-				return -EFAULT;
-			break;
-	}
-	return 0;
+        signed long long time;
+        switch (num)
+        {
+        case IOCTL_RST:
+                kbdKeyCount = 0;
+                kbdResetTime = ktime_get_real();
+                break;
+        case IOCTL_GET_CNT:
+                if (copy_to_user((unsigned long long)*param, &kbdKeyCount,
+                                        sizeof(unsigned long long)))
+                return -EFAULT;
+                break;
+        case IOCTL_GET_TIME:
+                time = ktime_to_ns(kbdResetTime);
+                if (copy_to_user((signed long long)*param, &time,
+                                        sizeof(signed long long)))
+                return -EFAULT;
+                break;
+        }
+        return 0;
 }
 
-static int kbdDeviceOpen(struct inode* inode, struct file* file)
+static int kbdDeviceOpen(struct inode *inode, struct file *file)
 {
-	if(kbdDeviceCount)
-		return -EBUSY;
-       	kbdDeviceCount++;	
-	try_module_get(THIS_MODULE);
-	return 0;
+        if (kbdDeviceCount)
+                return -EBUSY;
+        kbdDeviceCount++;
+        try_module_get(THIS_MODULE);
+        return 0;
 }
 
-static int kbdDeviceRelease(struct inode* inode, struct file* file)
+static int kbdDeviceRelease(struct inode *inode, struct file *file)
 {
-	kbdDeviceCount--;
-	module_put(THIS_MODULE);
-	return 0;
+        kbdDeviceCount--;
+        module_put(THIS_MODULE);
+        return 0;
 }
 
 struct file_operations kbdFileOps = {
-	.open = kbdDeviceOpen,
-	.release = kbdDeviceRelease,
-	.unlocked_ioctl = kbdDeviceIoctl, 
+        .open = kbdDeviceOpen,
+        .release = kbdDeviceRelease,
+        .unlocked_ioctl = kbdDeviceIoctl, 
 };
 
 static int __init kbdInit(void)
 {
-	int ret=0;
-	kbdKeyCount = 0;
-	kbdResetTime = ktime_get_real();
-	kbdMajor = register_chrdev(0, KBDCNT_NAME, &kbdFileOps);
-	if(kbdMajor<0)
-	{
-		printk(KERN_ALERT"Failed to register chrdev KeyboardCounter\n");
-		return kbdMajor;
+        int ret = 0;
+        kbdKeyCount = 0;
+        kbdResetTime = ktime_get_real();
+        kbdMajor = register_chrdev(0, KBDCNT_NAME, &kbdFileOps);
+        if (kbdMajor < 0) {
+                printk(KERN_ALERT"Failed to register chrdev KeyboardCounter\n");
+                return kbdMajor;
+        }
+        kbdClass = class_create(THIS_MODULE, KBDCNT_CLASS);
+        if (IS_ERR(kbdClass)) {
+                printk(KERN_ALERT"Failed to create device class KeyboardCounter\n");
+                unregister_chrdev(kbdMajor, KBDCNT_NAME);
+                return PTR_ERR(kbdClass);
+        }
+        kbdDevice = device_create(kbdClass, NULL, MKDEV(kbdMajor, 0),
+                        NULL, KBDCNT_DEVICE);
+        if (IS_ERR(kbdDevice)) {
+                printk(KERN_ALERT"Failed to create device KeyboardCounter\n");
+                class_destroy(kbdClass);
+                unregister_chrdev(kbdMajor, KBDCNT_NAME);
+                return PTR_ERR(kbdDevice);
 	}
-	kbdClass = class_create(THIS_MODULE, KBDCNT_CLASS);
-	if(IS_ERR(kbdClass))
-	{
-		printk(KERN_ALERT"Failed to create device class KeyboardCounter\n");
-		unregister_chrdev(kbdMajor, KBDCNT_NAME);
-		return PTR_ERR(kbdClass);
-	}
-	kbdDevice = device_create(kbdClass, NULL, MKDEV(kbdMajor, 0), NULL, KBDCNT_DEVICE);
-	if(IS_ERR(kbdDevice))
-	{
-		printk(KERN_ALERT"Failed to create device KeyboardCounter\n");
-		class_destroy(kbdClass);
-		unregister_chrdev(kbdMajor, KBDCNT_NAME);
-		return PTR_ERR(kbdDevice);
-	}
-        ret = request_irq(1, (irq_handler_t)kbdIrqHandler, IRQF_SHARED, KBDCNT_DEVICE, (void*)(kbdIrqHandler)); 
-	if(ret<0)
-	{
-		printk(KERN_ALERT"Failed to register KeyboardCounter interrupt\n");
-		device_destroy(kbdClass, kbdMajor);
-		class_destroy(kbdClass);
-		unregister_chrdev(kbdMajor, KBDCNT_NAME);
-		return ret;
-	}	
+        ret = request_irq(1, (irq_handler_t)kbdIrqHandler, IRQF_SHARED,
+                        KBDCNT_DEVICE, (void*)(kbdIrqHandler));
+        if (ret < 0) {
+                printk(KERN_ALERT"Failed to register KeyboardCounter interrupt\n");
+                device_destroy(kbdClass, kbdMajor);
+                class_destroy(kbdClass);
+                unregister_chrdev(kbdMajor, KBDCNT_NAME);
+                return ret;
+        }
 
-	printk(KERN_INFO"Loaded KeyboardCounter with major number: %d\n", kbdMajor);
-	return 0;
+        printk(KERN_INFO"Loaded KeyboardCounter with major number: %d\n",
+                        kbdMajor);
+        return 0;
 }
 
 static void __exit kbdExit(void)
 {
-	free_irq(1, (void*)(kbdIrqHandler));
-	device_destroy(kbdClass, MKDEV(kbdMajor, 0));
-	class_unregister(kbdClass);
-	class_destroy(kbdClass);
-	unregister_chrdev(kbdMajor, KBDCNT_NAME);
+        free_irq(1, (void*)(kbdIrqHandler));
+        device_destroy(kbdClass, MKDEV(kbdMajor, 0));
+        class_unregister(kbdClass);
+        class_destroy(kbdClass);
+        unregister_chrdev(kbdMajor, KBDCNT_NAME);
 }
 
 module_init(kbdInit);
